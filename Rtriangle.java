8176c863b22756b2e891e4b356965344cfd9d782
IT-school.com.ua <37939704+it-school@users.noreply.github.com>
2019-10-15 13:12:37 +0300

    Update Rtriangle.java

--- Rtriangle.java
+++ Rtriangle.java
@@ -2,7 +2,9 @@ package com.company;
 
 public class Rtriangle extends Triangl{
     public boolean Proverka(){
-        if ((a==b)&&(a==c)) return true;
+        super.Proverka();
+        if ((a==b)&&(a==c)) 
+            return true;
         return false;
     }
     public Rtriangle(double a,double b,double c){this.a=a;this.b=b;this.c=c;};
